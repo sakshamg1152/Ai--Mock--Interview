a514fafd3d810b8d4a9b33cd43c79106bcb4923d
sakshamg1152 <sakshamg1152@gmail.com>
2026-07-28 17:16:53 +0530

    Update server for Render deployment

--- backend/src/app.js
+++ backend/src/app.js
@@ -13,7 +13,6 @@ dotenv.config();
 
 const app=express();
 
-app.set("port" , (process.env.PORT || 8000));
 app.use(cors());
 app.use(express.json({limit : "40kb"}));
 app.use(express.urlencoded({limit : "40kb" , extended: true}))
@@ -24,13 +23,19 @@ app.use("/api/submission", submissionRouter);
 app.use("/api/dash" , homeRouter)
 app.use("/api/profile", profileRouter);
 
-const start = async ()=>{
-    const connectionDb = await mongoose.connect(process.env.MONGO_URL);
-    console.log("connected to DB")
-    app.listen(8000 , ()=>{
-        console.log("listening to the port");
-    });
-}
+const start = async () => {
+    try {
+        await mongoose.connect(process.env.MONGO_URL);
+        console.log("Connected to DB");
 
+        const PORT = process.env.PORT || 8000;
+
+        app.listen(PORT, () => {
+            console.log(`Server is running on port ${PORT}`);
+        });
+    } catch (err) {
+        console.log(err);
+    }
+};
 
 start();
